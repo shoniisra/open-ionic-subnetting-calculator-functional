1ca84cd13d2b1f7765da5293461db9fd5f3fdd2e
Shoniisra <jivillacis4@espe.edu.ec>
2019-12-08 19:14:06 -0500

    calculadora vlsm funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,20 @@
 import { Component } from "@angular/core";
-import { AlertController } from '@ionic/angular';
+import { AlertController } from "@ionic/angular";
 
-import { ToastController } from '@ionic/angular';
+import { ToastController } from "@ionic/angular";
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
-
 export class HomePage {
   ipAdress;
   calculate_button = 0;
+  ipvalida = true;
+  finvalida = true;
 
-  firstNetmask;
-  finalNetmask;
+  firstNetmask = 0;
+  finalNetmask = 0;
 
   firstBitsHost;
   finalBitsHost;
@@ -56,14 +57,29 @@ export class HomePage {
   tipo = "";
   myvalor = [];
 
-  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
 
   async presentAlert() {
     const alert = await this.alertController.create({
-      header: 'Autor:',
-      
-      message: ' Hecho con <ion-icon name="heart"></ion-icon> por <a href="https://github.com/shoniisra" color="light">Johnny Villacís</a>',
-      buttons: ['OK']
+      header: "Autor:",
+
+      message:
+        ' Hecho con <ion-icon name="heart"></ion-icon> por <a href="https://github.com/shoniisra" color="light">Johnny Villacís</a>',
+      buttons: ["OK"]
+    });
+
+    await alert.present();
+  }
+
+  async presentErrorAlert() {
+    const alert = await this.alertController.create({
+      header: "Error al Calcular",
+
+      message: "Datos erróneos, No se puede calcular",
+      buttons: ["Aceptar"]
     });
 
     await alert.present();
@@ -71,204 +87,314 @@ export class HomePage {
 
   async toastIp() {
     const toast = await this.toastController.create({
-      message: 'La dirección Ip que desea subnetear, ejemplo: 192.168.4.32',
-      mode:'ios',
-      color:'dark',
+      message: "La dirección Ip que desea subnetear, ejemplo: 192.168.4.32",
+      mode: "ios",
+      color: "dark",
       duration: 2000
     });
     toast.present();
   }
   async toastRed() {
     const toast = await this.toastController.create({
-      message: 'Las redes disponibles son: A < 127, B < 191, C < 223.',
-      mode:'ios',
-      color:'dark',
+      message:
+        "Este campo es llenado automáticamente por la aplicación. Las redes disponibles son: A < 127, B < 191, C < 223.",
+      mode: "ios",
+      color: "dark",
       duration: 2000
     });
     toast.present();
   }
   async toastNetInicio() {
     const toast = await this.toastController.create({
-      message: 'Este campo es llenado automáticamente por la aplicación según su dirección ip',
-      mode:'ios',
-      color:'dark',
+      message:
+        "Este campo es llenado automáticamente por la aplicación según su dirección ip",
+      mode: "ios",
+      color: "dark",
       duration: 2000
     });
     toast.present();
   }
   async toastNetFin() {
     const toast = await this.toastController.create({
-      message: 'La diferencia entre Netmask Final e Inicial es el número exponente que eleva a 2 dando como resultado las subredes a conseguir. Ejem: 2<sup>5</sup> = 32 subredes.',
-      mode:'ios',
-      color:'dark',
-       duration: 2000
+      message:
+        "La diferencia entre Netmask Final e Inicial es el número exponente que eleva a 2 dando como resultado las subredes a conseguir. Ejem: 2<sup>5</sup> = 32 subredes.",
+      mode: "ios",
+      color: "dark",
+      duration: 2000
     });
     toast.present();
   }
 
-  getIpNetmask() {
-    var myipAdress = this.ipAdress.split('.');
-    if (myipAdress[0] > 0 && myipAdress[0] < 127)
-    {
-        this.firstNetmask = 8;
-        this.tipo="Clase A";
+  validaNetmask() {
+    if (this.finalNetmask < this.firstNetmask) {
+      this.finvalida = true;
+    } else {
+      this.finvalida = false;
     }
-    else if (myipAdress[0] >= 128 && myipAdress[0] < 191)
-    {
-        this.firstNetmask = 16;
-        this.tipo="Clase B";
-    }
+  }
 
-    else if (myipAdress[0] >= 192 && myipAdress[0] < 223){
-        this.firstNetmask = 24;
-        this.tipo="Clase C";
+  getIpNetmask() {
+    this.validaNetmask();
+    if (
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+        this.ipAdress
+      )
+    ) {
+      this.ipvalida = false;
+    } else {
+      this.ipvalida = true;
     }
-    else{
-        this.firstNetmask = "No soportado";
+    var myipAdress = this.ipAdress.split(".");
+    if (myipAdress[0] > 0 && myipAdress[0] < 127) {
+      this.firstNetmask = 8;
+      this.tipo = "Clase A";
+    } else if (myipAdress[0] >= 128 && myipAdress[0] < 191) {
+      this.firstNetmask = 16;
+      this.tipo = "Clase B";
+    } else if (myipAdress[0] >= 192 && myipAdress[0] < 223) {
+      this.firstNetmask = 24;
+      this.tipo = "Clase C";
+    } else {
+      this.tipo = "No soportado";
+      this.ipvalida = true;
+      //  this.firstNetmask = "No soportado";
     }
     // this.firstNetmask.min=this.firstNetmask;
     // this.finalNetmask.min=this.firstNetmask;
     //el mínimo de la netmask de destino es la netmask por default
-}
-Limpiar(){
-  this.myvalor = [];
+  }
+  inicializar(){
 
-  this.subnettingAux1 = 0;
-  this.subnettingAux2 = 0;
-  this.subnettingAux3 = 0;
-  this.subnettingAux4 = 0;
+    this.subnettingAux1 = 0;
+    this.subnettingAux2 = 0;
+    this.subnettingAux3 = 0;
+    this.subnettingAux4 = 0;
 
-  this.subnetting1 = 0;
-  this.subnetting2 = 0;
-  this.subnetting3 = 0;
-  this.subnetting4 = 0;
+    this.subnetting1 = 0;
+    this.subnetting2 = 0;
+    this.subnetting3 = 0;
+    this.subnetting4 = 0;
 
-  this.firstIpAux1 = 0;
-  this.firstIpAux2 = 0;
-  this.firstIpAux3 = 0;
-  this.firstIpAux4 = 0;
+    this.firstIpAux1 = 0;
+    this.firstIpAux2 = 0;
+    this.firstIpAux3 = 0;
+    this.firstIpAux4 = 0;
 
-  this.finishIpAux1 = 0;
-  this.finishIpAux2 = 0;
-  this.finishIpAux3 = 0;
-  this.finishIpAux4 = 0;
+    this.finishIpAux1 = 0;
+    this.finishIpAux2 = 0;
+    this.finishIpAux3 = 0;
+    this.finishIpAux4 = 0;
 
-  this.broadcastAux1 = 0;
-  this.broadcastAux2 = 0;
-  this.broadcastAux3 = 0;
-  this.broadcastAux4 = 0;
+    this.broadcastAux1 = 0;
+    this.broadcastAux2 = 0;
+    this.broadcastAux3 = 0;
+    this.broadcastAux4 = 0;
 
-  this.total = 1; //variable que dice cuántas redes hay
-  this.vali = 0;
-}
+    this.total = 1; //variable que dice cuántas redes hay
+    this.vali = 0;
+
+  }
+  Limpiar() {
+    //Variables Front End
+    this.ipAdress = "0.0.0.0";
+    this.tipo = "";
+    this.firstNetmask = 0;
+    this.finalNetmask = 0;
+    this.myvalor = [];
+
+    this.ipvalida = true;
+    this.inicializar();
+   
+  }
 
   Calcular() {
-    this.getIpNetmask();
-    if (this.firstNetmask != "No soportado") {
-      this.firstBitsHost = 32 - this.firstNetmask;
-      this.firstIpNumber = Math.pow(2, this.firstBitsHost);
+    //this.getIpNetmask();
+    if (
+      this.tipo == "No soportado" ||
+      this.ipvalida == true ||
+      this.finvalida == true
+    ) {
+      this.presentErrorAlert();
+      return;
+    }
 
-      this.finalBitsHost = 32 - this.finalNetmask;
-      this.finalIpNumber = Math.pow(2, this.finalBitsHost);
+    this.firstBitsHost = 32 - this.firstNetmask;
+    this.firstIpNumber = Math.pow(2, this.firstBitsHost); //numero de host en la netmask inicial
 
-      this.subnettingNumber = this.firstIpNumber / this.finalIpNumber;
-      this.subnettingSize = this.firstIpNumber / this.subnettingNumber;
+    this.finalBitsHost = 32 - this.finalNetmask;
+    this.finalIpNumber = Math.pow(2, this.finalBitsHost); //numero de host en la netmask final
 
-      this.Limpiar();
-     
+    this.subnettingNumber = this.firstIpNumber / this.finalIpNumber; //numero de subredes creadas
+    this.subnettingSize = this.firstIpNumber / this.subnettingNumber; //numero de host en cada subred
 
-      this.octetos = this.ipAdress.split(".");
+    this.octetos = this.ipAdress.split(".");
 
-      for (let i = 1; i <= this.subnettingNumber; i++) {
-        this.subnettingAux4 += this.subnettingSize;
-        if (this.subnettingAux4 > 255) {
-          this.val = this.subnettingAux4 / 256;
-          this.subnettingAux3 += Math.round(this.val);
-          this.subnettingAux4 = 0;
-        }
-        if (this.subnettingAux3 > 255) {
-          this.val = this.subnettingAux3 / 256;
-          this.subnettingAux2 += Math.round(this.val);
-          this.subnettingAux3 = 0;
-          this.subnettingAux4 = 0;
-        }
+    this.inicializar();
+    if(this.subnettingNumber>1000){
+      alert("Detenido en 1000 subnets, HOST existentes: "+this.subnettingNumber);
+      this.subnettingNumber=1000;
+    }
 
-        // Calculo de Broadcast
-        this.broadcastAux1 = this.subnettingAux1;
-        this.broadcastAux2 = this.subnettingAux2;
-        this.broadcastAux3 = this.subnettingAux3;
-        this.broadcastAux4 = this.subnettingAux4 - 1;
-        if (this.subnettingAux4 == 0) {
-          if (this.subnettingAux3 == 0) {
-            this.broadcastAux2 = this.subnettingAux2 - 1;
-            this.broadcastAux3 = 255;
-            this.broadcastAux4 = 255;
-          } else {
-            this.broadcastAux3 = this.subnettingAux3 - 1;
-            this.broadcastAux4 = 255;
-          }
-        }
-
-        // Calculo de IpFinal
-        this.finishIpAux1 = this.broadcastAux1;
-        this.finishIpAux2 = this.broadcastAux2;
-        this.finishIpAux3 = this.broadcastAux3;
-        this.finishIpAux4 = this.broadcastAux4 - 1;
-
-        this.firstIpAux1 = this.subnetting1;
-        this.firstIpAux2 = this.subnetting2;
-        this.firstIpAux3 = this.subnetting3;
-        this.firstIpAux4 = this.subnetting4 + 1;
-
-        if (this.tipo == "Clase A") {
-          if (this.vali == 0)
-            this.total =
-              (this.finishIpAux2 + 1) *
-                (this.finishIpAux3 + 1) *
-                (this.finishIpAux4 + 2) -
-              2;
-          this.vali++;
-          this.myvalor[i-1] = [
-            i,
-            this.octetos[0]+"."+this.subnetting2+"."+this.subnetting3+"."+this.subnetting4,
-            this.octetos[0]+"."+this.firstIpAux2+"."+this.firstIpAux3+"."+this.firstIpAux4,
-            this.octetos[0]+"."+this.finishIpAux2+"."+this.finishIpAux3+"."+this.finishIpAux4,
-            this.octetos[0]+"."+this.broadcastAux2+"."+this.broadcastAux3+"."+this.broadcastAux4,
-            this.total
-          ];
-        } else if (this.tipo == "Clase B") {
-          if (this.vali == 0)
-            this.total = (this.finishIpAux3 + 1) * (this.finishIpAux4 + 2) - 2;
-          this.vali++;
-          this.myvalor[i-1] = [
-            i,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.subnetting3+"."+this.subnetting4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.firstIpAux3+"."+this.firstIpAux4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.finishIpAux3+"."+this.finishIpAux4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.broadcastAux3+"."+this.broadcastAux4,
-            this.total
-          ];
-        } else if (this.tipo == "Clase C") {
-          if (this.vali == 0) this.total = this.finishIpAux4 + 2 - 2;
-          this.vali++;
-
-          this.myvalor[i-1] = [
-            i,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.octetos[2]+"."+this.subnetting4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.octetos[2]+"."+this.firstIpAux4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.octetos[2]+"."+this.finishIpAux4,
-            this.octetos[0]+"."+this.octetos[1]+"."+this.octetos[2]+"."+this.broadcastAux4,
-            this.total
-          ];
-        } else {
-        }
-        this.subnetting1 = this.subnettingAux1;
-        this.subnetting2 = this.subnettingAux2;
-        this.subnetting3 = this.subnettingAux3;
-        this.subnetting4 = this.subnettingAux4;
-        console.log(this.myvalor[i]);
+    for (let i = 1; i <= this.subnettingNumber; i++) {
+      this.subnettingAux4 += this.subnettingSize;
+      if (this.subnettingAux4 > 255) {
+        this.val = this.subnettingAux4 / 256;
+        this.subnettingAux3 += Math.round(this.val);
+        this.subnettingAux4 = 0;
       }
-     
-    } else alert("no se puede calcular");
+      if (this.subnettingAux3 > 255) {
+        this.val = this.subnettingAux3 / 256;
+        this.subnettingAux2 += Math.round(this.val);
+        this.subnettingAux3 = 0;
+        this.subnettingAux4 = 0;
+      }
+
+      // Calculo de Broadcast
+      this.broadcastAux1 = this.subnettingAux1;
+      this.broadcastAux2 = this.subnettingAux2;
+      this.broadcastAux3 = this.subnettingAux3;
+      this.broadcastAux4 = this.subnettingAux4 - 1;
+      if (this.subnettingAux4 == 0) {
+        if (this.subnettingAux3 == 0) {
+          this.broadcastAux2 = this.subnettingAux2 - 1;
+          this.broadcastAux3 = 255;
+          this.broadcastAux4 = 255;
+        } else {
+          this.broadcastAux3 = this.subnettingAux3 - 1;
+          this.broadcastAux4 = 255;
+        }
+      }
+
+      // Calculo de IpFinal
+      this.finishIpAux1 = this.broadcastAux1;
+      this.finishIpAux2 = this.broadcastAux2;
+      this.finishIpAux3 = this.broadcastAux3;
+      this.finishIpAux4 = this.broadcastAux4 - 1;
+
+      this.firstIpAux1 = this.subnetting1;
+      this.firstIpAux2 = this.subnetting2;
+      this.firstIpAux3 = this.subnetting3;
+      this.firstIpAux4 = this.subnetting4 + 1;
+
+      if (this.tipo == "Clase A") {
+        if (this.vali == 0)
+          this.total =
+            (this.finishIpAux2 + 1) *
+              (this.finishIpAux3 + 1) *
+              (this.finishIpAux4 + 2) -
+            2;
+        this.vali++;
+        this.myvalor[i - 1] = [
+          i,
+          this.octetos[0] +
+            "." +
+            this.subnetting2 +
+            "." +
+            this.subnetting3 +
+            "." +
+            this.subnetting4,
+          this.octetos[0] +
+            "." +
+            this.firstIpAux2 +
+            "." +
+            this.firstIpAux3 +
+            "." +
+            this.firstIpAux4,
+          this.octetos[0] +
+            "." +
+            this.finishIpAux2 +
+            "." +
+            this.finishIpAux3 +
+            "." +
+            this.finishIpAux4,
+          this.octetos[0] +
+            "." +
+            this.broadcastAux2 +
+            "." +
+            this.broadcastAux3 +
+            "." +
+            this.broadcastAux4,
+          this.total
+        ];
+      } else if (this.tipo == "Clase B") {
+        if (this.vali == 0)
+          this.total = (this.finishIpAux3 + 1) * (this.finishIpAux4 + 2) - 2;
+        this.vali++;
+        this.myvalor[i - 1] = [
+          i,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.subnetting3 +
+            "." +
+            this.subnetting4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.firstIpAux3 +
+            "." +
+            this.firstIpAux4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.finishIpAux3 +
+            "." +
+            this.finishIpAux4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.broadcastAux3 +
+            "." +
+            this.broadcastAux4,
+          this.total
+        ];
+      } else if (this.tipo == "Clase C") {
+        if (this.vali == 0) this.total = this.finishIpAux4 + 2 - 2;
+        this.vali++;
+
+        this.myvalor[i - 1] = [
+          i,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.octetos[2] +
+            "." +
+            this.subnetting4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.octetos[2] +
+            "." +
+            this.firstIpAux4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.octetos[2] +
+            "." +
+            this.finishIpAux4,
+          this.octetos[0] +
+            "." +
+            this.octetos[1] +
+            "." +
+            this.octetos[2] +
+            "." +
+            this.broadcastAux4,
+          this.total
+        ];
+      } else {
+      }
+      this.subnetting1 = this.subnettingAux1;
+      this.subnetting2 = this.subnettingAux2;
+      this.subnetting3 = this.subnettingAux3;
+      this.subnetting4 = this.subnettingAux4;
+      console.log(this.myvalor[i]);
+    }
   }
 }
--- src/app/pages/lista/lista.page.ts
+++ src/app/pages/lista/lista.page.ts
@@ -1,15 +1,276 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+import { AlertController } from "@ionic/angular";
+
+import { ToastController } from "@ionic/angular";
 @Component({
-  selector: 'app-lista',
-  templateUrl: './lista.page.html',
-  styleUrls: ['./lista.page.scss'],
+  selector: "app-lista",
+  templateUrl: "./lista.page.html",
+  styleUrls: ["./lista.page.scss"]
 })
-export class ListaPage implements OnInit {
+export class ListaPage {
+  ipAdress;
+  tipo = "";
+  firstNetmask;
+  listahost = "";
+  calculate_button = 0;
+  ipvalida = true;
+  finvalida = true;
 
-  constructor() { }
+  equivalencias = [
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128,
+    256,
+    512,
+    1024,
+    2048,
+    4096,
+    8192,
+    16384,
+    32768,
+    65536,
+    131072,
+    262144,
+    524288,
+    1048576
+  ];
 
-  ngOnInit() {
+  cambio = 0;
+  total;
+  vali;
+  val;
+  octetos;
+  hsolitados = [];
+  myvalor = [];
+  direccion;
+  mascara = 0;
+
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: "Autor:",
+
+      message:
+        ' Hecho con <ion-icon name="heart"></ion-icon> por <a href="https://github.com/shoniisra" color="light">Johnny Villacís</a>',
+      buttons: ["OK"]
+    });
+
+    await alert.present();
   }
 
+  async presentErrorAlert(mystr) {
+    const alert = await this.alertController.create({
+      header: "Error al Calcular",
+
+      message: mystr,
+      buttons: ["Aceptar"]
+    });
+
+    await alert.present();
+  }
+
+  async toastIp() {
+    const toast = await this.toastController.create({
+      message: "La dirección Ip que desea subnetear, ejemplo: 192.168.4.32",
+      mode: "ios",
+      color: "dark",
+      duration: 2000
+    });
+    toast.present();
+  }
+  async toastRed() {
+    const toast = await this.toastController.create({
+      message:
+        "Este campo es llenado automáticamente por la aplicación. Las redes disponibles son: A < 127, B < 191, C < 223.",
+      mode: "ios",
+      color: "dark",
+      duration: 2000
+    });
+    toast.present();
+  }
+  async toastNetInicio() {
+    const toast = await this.toastController.create({
+      message:
+        "Este campo es llenado automáticamente por la aplicación según su dirección ip",
+      mode: "ios",
+      color: "dark",
+      duration: 2000
+    });
+    toast.present();
+  }
+  async toastNetFin() {
+    const toast = await this.toastController.create({
+      message: "El numero de host decide el tamaño de cada subred.",
+      mode: "ios",
+      color: "dark",
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  validaListaHost() {
+    if (this.listahost == "") {
+      this.finvalida = true;
+    } else {
+      if (!/[^0-9,]/.test(this.listahost)) {
+        this.finvalida = false;
+      } else {
+        this.finvalida = true;
+      }
+    }
+
+    var mylista: any = this.listahost.split(",");
+    mylista = mylista.join("");
+  }
+
+  getIpNetmask() {
+    this.validaListaHost();
+    if (
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+        this.ipAdress
+      )
+    ) {
+      this.ipvalida = false;
+    } else {
+      this.ipvalida = true;
+    }
+    var myipAdress = this.ipAdress.split(".");
+    if (myipAdress[0] > 0 && myipAdress[0] < 127) {
+      this.firstNetmask = 8;
+      this.tipo = "Clase A";
+    } else if (myipAdress[0] >= 128 && myipAdress[0] < 191) {
+      this.firstNetmask = 16;
+      this.tipo = "Clase B";
+    } else if (myipAdress[0] >= 192 && myipAdress[0] < 223) {
+      this.firstNetmask = 24;
+      this.tipo = "Clase C";
+    } else {
+      this.tipo = "No soportado";
+      this.ipvalida = true;
+      //  this.firstNetmask = "No soportado";
+    }
+    // this.firstNetmask.min=this.firstNetmask;
+    // this.finalNetmask.min=this.firstNetmask;
+    //el mínimo de la netmask de destino es la netmask por default
+  }
+  inicializar() {
+    this.total = 1; //variable que dice cuántas redes hay
+    this.vali = 0;
+    this.cambio = 0;
+    this.direccion = [];
+    this.mascara = 0;
+  }
+  Limpiar() {
+    //Variables Front End
+    this.ipAdress = "0.0.0.0";
+    this.tipo = "";
+    this.listahost = "";
+    this.firstNetmask = 0;
+    this.myvalor = [];
+    this.ipvalida = true;
+    this.finvalida = true;
+    this.inicializar();
+  }
+  CalcularDireccionRed(ipactual, salto) {
+    ipactual[3] += salto;
+    if (ipactual[3] > 255) {
+      this.val = ipactual[3] / 256;
+      ipactual[2] += Math.round(this.val);
+      ipactual[3] = 0;
+    }
+    if (ipactual[2] > 255) {
+      this.val = ipactual[2] / 256;
+      ipactual[1] += Math.round(this.val);
+      ipactual[2] = 0;
+      ipactual[3] = 0;
+    }
+    if (ipactual[1] > 255) {
+      this.val = ipactual[1] / 256;
+      ipactual[0] += Math.round(this.val);
+      ipactual[1] = 0;
+      ipactual[2] = 0;
+      ipactual[3] = 0;
+    }
+    return ipactual;
+  }
+
+  Calcular() {
+    this.getIpNetmask();
+    if (
+      this.tipo == "No soportado" ||
+      this.ipvalida == true ||
+      this.finvalida == true ||
+      this.listahost == ""
+    ) {
+      this.presentErrorAlert("Datos erróneos, No se puede calcular");
+      return;
+    }
+    this.octetos = this.ipAdress.split(".").map(function(item) {
+      return parseInt(item, 10);
+    });
+    this.hsolitados = this.listahost.split(",").map(function(item) {
+      return parseInt(item, 10);
+    }); //transformo 123,123,123 a array
+
+    var suma = 0;
+    this.hsolitados.forEach(function(numero) {
+      suma += numero;
+    });
+    if (suma >= 16581375) {
+      this.presentErrorAlert("Los Host Solicitados Exceden al máximo disponibles");
+      return;
+    }
+    this.hsolitados = this.hsolitados.sort((a, b) => b - a); //ordeno de mayor a menor
+    this.inicializar();
+    this.direccion = this.octetos;
+    this.cambio = 0;
+    for (let index = 0; index < this.hsolitados.length; index++) {
+      for (let i = 0; i < this.equivalencias.length; i++) {
+        if (this.hsolitados[index] <= this.equivalencias[i] - 2) {
+          //numero--hsolicitados--hencontrados--direcciondered -- mascara-- primeraip--ultimaip
+          console.log(this.direccion);
+          console.log(this.cambio);
+          this.direccion = this.CalcularDireccionRed(
+            [...this.direccion],
+            this.cambio
+          );
+          var aux = this.CalcularDireccionRed([...this.direccion], 1);
+          this.cambio = this.equivalencias[i];
+          this.mascara = this.firstNetmask;
+          var nuevamascara = 32 - i;
+
+          var aux2 = this.CalcularDireccionRed(
+            [...this.direccion],
+            this.cambio - 2
+          );
+          var aux3 = this.CalcularDireccionRed(
+            [...this.direccion],
+            this.cambio - 1
+          );
+          this.myvalor[index] = [
+            index + 1,
+            this.hsolitados[index],
+            this.equivalencias[i] - 2,
+            this.direccion.join("."),
+            nuevamascara,
+            aux.join("."),
+            aux2.join("."),
+            aux3.join(".")
+          ];
+          break;
+        }
+      }
+    }
+    console.log(this.myvalor);
+  }
 }
